48506be46ffd118a418c2b0d2c18a468bbf48995
Kagiso Monene <kagisothierry31@gmail.com>
2025-09-22 08:48:55 +0200

    Added typing feature

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { SocialLinks } from './SocialLinks';
 
 interface HeroSectionProps {
@@ -6,6 +6,42 @@ interface HeroSectionProps {
 }
 
 const HeroSection: React.FC<HeroSectionProps> = ({ scrollY }) => {
+    const [text, setText] = useState('');
+    const [loopNum, setLoopNum] = useState(0);
+    const [isDeleting, setIsDeleting] = useState(false);
+    const [delta, setDelta] = useState(200 - Math.random() * 100);
+
+    const rolesToRotate = ["Frontend Developer"];
+    const period = 2000;
+
+    useEffect(() => {
+        const tick = () => {
+            let i = loopNum % rolesToRotate.length;
+            let fullText = rolesToRotate[i];
+            let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+
+            setText(updatedText);
+
+            if (isDeleting) {
+                setDelta(prevDelta => prevDelta / 2);
+            }
+
+            if (!isDeleting && updatedText === fullText) {
+                setIsDeleting(true);
+                setDelta(period);
+            } else if (isDeleting && updatedText === '') {
+                setIsDeleting(false);
+                setLoopNum(loopNum + 1);
+                setDelta(500);
+            }
+        };
+
+        let ticker = setTimeout(() => {
+            tick();
+        }, delta);
+
+        return () => { clearTimeout(ticker); };
+    }, [text, delta, isDeleting, loopNum]);
     return (
         <section
             id="hero"
@@ -26,6 +62,7 @@ const HeroSection: React.FC<HeroSectionProps> = ({ scrollY }) => {
                 <h1 className="text-6xl md:text-8xl lg:text-8xl font-anton uppercase tracking-widest mb-4">
                     Kagiso Monene<span className="text-white-400">.</span>
                 </h1>
+                <h3 className="text-lg text-text-color dark:text-gray-300 h-7">{text}<span className="border-r-2 border-text-color dark:border-gray-300 animate-pulse" aria-hidden="true"></span></h3>
                 <p className="text-lg md:text-2xl text-neutral-300 max-w-2xl mx-auto">
                     A tech creator turning ideas into interactive web experiences and AI-powered tools. I make learning smarter, digital solutions simpler, and projects that leave an impact. Dive in and see what's possible.
                 </p>
